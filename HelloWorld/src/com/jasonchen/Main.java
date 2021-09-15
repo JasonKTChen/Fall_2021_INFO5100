f4c458def3658feb7e9c09430da9b2811f17c4b7
JasonKTChen <49644422+JasonKTChen@users.noreply.github.com>
2021-09-14 21:21:15 -0700

    lession one

---
+++ HelloWorld/src/com/jasonchen/Main.java
@@ -0,0 +1,16 @@
+package com.jasonchen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        short maxshort = Short.MAX_VALUE;
+        System.out.println(maxshort);
+
+
+
+
+    }
+
+
+}
